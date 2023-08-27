b7d042f348c6fb154aa1d7048a7a71613737cc85
marcosviniciusbarbosa <marcosvb-oficial@outlook.com>
2023-08-27 20:13:25 -0300

    projeto

--- site-angular/src/app/app-routing.module.ts
+++ site-angular/src/app/app-routing.module.ts
@@ -2,13 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { PerfilComponent } from './perfil/perfil.component';
 import { HomeComponent } from './home/home.component';
+import { ItemComponent } from './item/item.component';
 
 const routes: Routes = [
   { path: "", component: HomeComponent},
-  { path: "perfil", component: PerfilComponent}
+  { path: "perfil", component: PerfilComponent},
+  { path: "item", component: ItemComponent}
 ];
-
-
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ site-angular/src/app/item/item.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-item',
+  templateUrl: './item.component.html',
+  styleUrls: ['./item.component.scss']
+})
+export class ItemComponent {
+  itens = [
+    'assets/img/pc1.jpg'
+  ]
+
+}
