0dc097a7fb3765183515e214fe82e5ba2036e008
tww577 <pvzgw2isgood@gmail.com>
2023-08-28 14:03:18 -0300

    uyk

--- site-angular/src/app/app-routing.module.ts
+++ site-angular/src/app/app-routing.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { PerfilComponent } from './perfil/perfil.component';
 import { HomeComponent } from './home/home.component';
+import { ContatoComponent } from './contato/contato.component';
+import { ServicosComponent } from './servicos/servicos.component';
+import { SobreComponent } from './sobre/sobre.component';
 
 const routes: Routes = [
   { path: "", component: HomeComponent},
-  { path: "perfil", component: PerfilComponent}
+  { path: "perfil", component: PerfilComponent},
+  { path: "contato", component: ContatoComponent},
+  { path: "servicos", component: ServicosComponent},
+  { path: "sobre", component: SobreComponent}
 ];
 
 
